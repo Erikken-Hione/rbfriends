2898e9160d6203056d08c59cd6fecb8b9e9f5ef6
Erikken-Hione <68046024+Erikken-Hione@users.noreply.github.com>
2020-08-03 14:39:39 +0600

    Updates

---
+++ precache-manifest.c554ea348f882057057162860c30f9e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d665facb9c66608ba722421c7ae26b0",
+    "url": "/rbfriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/rbfriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f59463661d37d68613fd",
+    "url": "/rbfriends/static/css/main.02da5706.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/rbfriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rbfriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f59463661d37d68613fd",
+    "url": "/rbfriends/static/js/main.74afff52.chunk.js"
+  },
+  {
+    "revision": "5881e4019a247099f798",
+    "url": "/rbfriends/static/js/runtime-main.73b18ac4.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/rbfriends/static/media/SEGA.82e09f78.TTF"
+  }
+]);
